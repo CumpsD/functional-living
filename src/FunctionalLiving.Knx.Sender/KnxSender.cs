7c6f8c066e428356388c06bcce74ff59495391d2
David Cumps <david@cumps.be>
2020-06-16 14:55:50 +0200

    refactor: add more logging

--- src/FunctionalLiving.Knx.Sender/KnxSender.cs
+++ src/FunctionalLiving.Knx.Sender/KnxSender.cs
@@ -176,7 +176,7 @@ namespace FunctionalLiving.Knx.Sender
             KnxAddress destinationAddress,
             byte[] state)
             => _logger.LogInformation(
-                "{sourceAddress} - {destinationAddress} - {state}",
+                "From Device '{SourceAddress}' to Group '{DestinationAddress}': '{State}'",
                 sourceAddress.ToString(),
                 destinationAddress.ToString(),
                 BitConverter.ToString(state));

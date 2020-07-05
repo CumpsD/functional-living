43d460baf4c22c53a69a7de7047876ecbfa22165
David Cumps <david@cumps.be>
2020-07-05 15:35:01 +0200

    fix: waitfor can be mising WriteTo section

--- src/FunctionalLiving.Knx.Listener/Infrastructure/WaitFor.cs
+++ src/FunctionalLiving.Knx.Listener/Infrastructure/WaitFor.cs
@@ -30,7 +30,7 @@ namespace FunctionalLiving.Knx.Listener.Infrastructure
                 .GetSection("Serilog:WriteTo")
                 .Get<WriteTo[]>();
 
-            var seq = writeTos.SingleOrDefault(to => "Seq".Equals(to.Name, StringComparison.InvariantCultureIgnoreCase));
+            var seq = writeTos?.SingleOrDefault(to => "Seq".Equals(to.Name, StringComparison.InvariantCultureIgnoreCase));
             if (seq == null)
                 return;
 
